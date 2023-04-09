28532bc13d0ac8eba557428af5ebc8edec49e098
NPare <NPare@EN1710346>
2023-04-09 12:49:05 -0300

    Add project files.

---
+++ Ejercicio 8.1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Solicite al usuario el ingreso de una frase y presente un mensaje indicando si alguna contiene la palabra “fin”.
+string frase = "";
+Console.WriteLine("Por favor, ingrese una frase");
+frase = Console.ReadLine();
+
+string[] valores = frase.Split(" ");
+
+for (int i = 0; i < valores.Length; i++)
+    if (valores[i] == "fin")
+    {
+        Console.WriteLine("Existe fin");
+    }
+
+Console.ReadKey();
